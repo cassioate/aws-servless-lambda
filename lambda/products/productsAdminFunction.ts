2a788ae7ce2736d8dbbce936c7b0ba4aa4752542
Cassio <cassioate@gmail.com>
2022-11-21 21:05:07 -0300

    feat: api get post put delete of products with dynamoDb

--- lambda/products/productsAdminFunction.ts
+++ lambda/products/productsAdminFunction.ts
@@ -9,45 +9,60 @@ const productRepository = new ProductRepository(ddbClient, productsDdb)
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
   const method = event.httpMethod
-
+  
   const lambdaRequestId = context.awsRequestId;
   const apiRequestId = event.requestContext.requestId;
   
   console.log(`API Gateway RequestID: ${apiRequestId} - Lambda execution RequestID: ${lambdaRequestId}`)
-
-    if (event.resource === "/products" && method === "POST") {
-      console.log('POST')
-      const product = JSON.parse(event.body!) as Product
-      const productSaved = await productRepository.create(product)
-      return Promise.resolve({
-        statusCode: 201,
-        body: JSON.stringify(productSaved)
-      })
-    }
   
-    else if (event.resource === "/products/{id}" && method === "PUT") {
-      const productId = event.pathParameters!.id as string;
+  
+  if (event.resource === "/products" && method === "POST") {
+    console.log('POST')
+    const product = JSON.parse(event.body!) as Product
+    const productSaved = await productRepository.create(product)
+    return Promise.resolve({
+      statusCode: 201,
+      body: JSON.stringify(productSaved)
+    })
+  }
+  
+  else if (event.resource === "/products/{id}") {
+    const productId = event.pathParameters!.id as string;
+    if (method === "PUT") {
+      const product = JSON.parse(event.body!) as Product
       console.log(`PUT products/${productId}`)
   
-      return Promise.resolve({
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `PUT products/${productId}`
+      try {
+        const productSaved = await productRepository.updateProduct(productId, product)
+        return Promise.resolve({
+          statusCode: 200,
+          body: JSON.stringify(productSaved)
         })
-      })
+      } catch (error) {
+        return Promise.resolve({
+          statusCode: 404,
+          body: 'Product not found'
+        })
+      }
     }
+    else if (method === "DELETE") {
+        console.log(`DELETE products/${productId}`)
 
-    else if (event.resource === "/products/{id}" && method === "DELETE") {
-      const productId = event.pathParameters!.id as string;
-      console.log(`DELETE products/${productId}`)
-  
-      return Promise.resolve({
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `DELETE products/${productId}`
-        })
-      })
+        try {
+          const productDeleted = await productRepository.deleteProduct(productId)
+          return Promise.resolve({
+            statusCode: 200,
+            body: JSON.stringify(productDeleted)
+          })
+        } catch (error) {
+          console.error((<Error>error).message)
+          return Promise.resolve({
+            statusCode: 404,
+            body: (<Error>error).message
+          })
+        }
     }
+  }
 
   return Promise.resolve({
     statusCode: 400,
